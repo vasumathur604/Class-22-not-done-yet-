97678b5276d3bfaffc31e1015675e90176e1d5fe
vasumathur604 <78646185+vasumathur604@users.noreply.github.com>
2021-04-30 09:22:35 +0530

    Add files via upload
    Mam i am facing some problem .I don't know how to correct it.

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var starImg,bgImg;
+var star, starBody;
+var Sound
+//create variable for fairy sprite and fairyImg
+var fairy,fairyBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	//load animation for fairy here
+	fairyImg=loadAnimation("images/fairyImage1.png","images/fairyImage2.png")
+
+   //load sound 
+//Sound = loadSound("JoyMusic.mp3")
+}
+
+
+function setup() {
+	createCanvas(800, 750);
+
+	//write code to play fairyVoice sound
+
+	//create fairy sprite and add animation for fairy
+	fairy = createSprite(150,590);
+	fairy.addAnimation(fairyImg);
+	
+
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5,isStatic:true});
+	World.add(world, starBody);
+	
+	fairyBody = Bodies.circle(150,590,)
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  //write code to stop star in the hand of fairy
+if(star.y> 470 && starBody.position.y > 470 ){
+	Matter.Body.setStatic(starBody,true);
+}
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//write code to move fairy left and right
+	if (keyCode === RIGHT_ARROW) {
+		fairy.x = fairy.x + 10 
+	}
+
+	if (keyCode === LEFT_ARROW) {
+	fairy.x = fairy.x - 10
+	}
+	
+}
